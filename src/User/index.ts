7a48a6584377868b03623a51dca3ac531cf92e29
Daso <daso.timileyin@gmail.com>
2022-01-12 16:03:24 +0100

    updates

--- src/User/index.ts
+++ src/User/index.ts
@@ -1,7 +1,9 @@
 import express from 'express'
-import {  createUser } from './user.controller';
+import {  createUser, deleteUser, updateUser } from './user.controller';
 const userRoute = express.Router()
 
 userRoute.post('/user', createUser)
+userRoute.put('/user/:id', updateUser)
+userRoute.delete('/user/:id', deleteUser)
 
 export { userRoute}
--- src/User/user.controller.ts
+++ src/User/user.controller.ts
@@ -31,4 +31,43 @@ const createUser =async (req:Request, res: Response) => {
     }
 }
 
-export { createUser}
+const updateUser = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+    const { enabled, fullName, role } = req.body;
+  
+    const user = await User.findById(id);
+  
+    if (!user) {
+      return res.status(404).json({ message: `User with id "${id}" not found.` });
+    }
+  
+    if (!fullName || !role) {
+      return res.status(422).json({ message: 'The fields fullName and role are required' });
+    }
+  
+    await User.updateOne({ _id: id }, { enabled, fullName, role });
+  
+    const userUpdated = await User.findById(id);
+  
+    return res.status(200).json({ data: userUpdated });
+    } catch (error: unknown) {
+        return res.json(error);
+    }
+  };
+  
+  const deleteUser = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+  
+    await User.findByIdAndDelete(id);
+  
+    return res.status(200).json({ message: 'User deleted successfully.' });
+    } catch (error: unknown) {
+        return res.json(error);
+    }
+  };
+  
+  //export { createUser, deleteUser, getAllUsers, getUser, updateUser };
+
+export { createUser, deleteUser, updateUser}
